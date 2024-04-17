872b3382b6d24068aa7d7b5078694ff2b9ce08b7
auadmendes <luciano.auad@gmail.com>
2024-04-17 12:25:39 -0300

    Adding puppeteer core

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,17 @@
 import express from 'express';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core'; // Import puppeteer-core instead of puppeteer
 
 const routes = express.Router();
 
 routes.get('/events', async (req, res) => {
+    const ingressoDigital = 'https://www.ingressodigital.com/list.php?busca=S&txt_busca=Vit%C3%B3ria&txt_busca_m=';
 
-    console.log('passando...')
+    // Specify the path to Chromium executable on the server
+    const browser = await puppeteer.launch({
+        executablePath: '/path/to/your/chromium/executable',
+        headless: true, // Or set it to false for debugging
+    });
 
-
-    const ingressoDigital = 'https://www.ingressodigital.com/list.php?busca=S&txt_busca=Vit%C3%B3ria&txt_busca_m='
-    const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage();
 
     await page.goto(ingressoDigital);
@@ -18,14 +20,9 @@ routes.get('/events', async (req, res) => {
         return divs.map(div => div.textContent?.trim());
     });
 
-    console.log('Text inside divs with class col-6:', textContents);
-
     await browser.close();
 
     return res.json({ textContents });
-
-
-
 });
 
 export default routes;
